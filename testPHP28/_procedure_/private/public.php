97144bbbce2aa0d1719e66c446d9bc24d19dbb29
Debaprasad Tapader <dev.openroot@gmail.com>
2025-01-18 20:51:41 +0530

    submit

--- testPHP28/_procedure_/private/public.php
+++ testPHP28/_procedure_/private/public.php
@@ -1,3 +1,9 @@
 <?php
+	/*
+		This is to implement protocol in the default scenario to start quick development.
+		Typically, it tries to avoid executional errors at resource unavailability.
+
+		It aggregates the "private/public" alternative for "private/protected" development architecture.
+	*/
 	require_once("private/public/mysql/configurationMysql.php");
 ?>
